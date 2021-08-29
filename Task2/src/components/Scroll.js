e03326547260fc4c2877b6bc26508d7a17d86544
Sanket Sambhaji Patil <73840483+SanketPatil5248@users.noreply.github.com>
2021-08-29 10:57:03 +0530

    Add files via upload

---
+++ Task2/src/components/Scroll.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Scroll = (props) => {
+  return (
+    //used to provide a scroll view
+    <div style={{ overflowY: "hidden" }}>{props.children}</div>
+  );
+};
+
+export default Scroll;
---
+++ Task2/src/components/SearchBox.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./search.css";
+
+const SearchBox = ({ searchfeild, searchChange }) => {
+  return (
+    <div className="pa3">
+      <input
+        className="pa2 ba"
+        type="search"
+        id="sear"
+        placeholder="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Search User"
+        onChange={searchChange}
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
